ed58f6aea2dce48d9e2f228b478d0a98c8d84e11
Axaka <martinvadseth@gmail.com>
2016-09-26 13:47:50 +0200

    Added script to move the player.

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+	[SerializeField]
+	float jumpForce = 10f;
+
+	[SerializeField]
+	float moveForce = 10f;
+
+	Rigidbody2D rb;
+
+	bool grounded = false;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.AddForce(Vector2.right * Input.GetAxisRaw("Horizontal") * moveForce * Time.deltaTime, ForceMode2D.Impulse);
+
+		if (grounded && Input.GetAxisRaw("Vertical") != 0)
+		{
+			rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+			grounded = false;
+		}
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		grounded = true;
+	}
+
+	void OnCollisionExit2D(Collision2D col)
+	{
+		grounded = false;
+	}
+}

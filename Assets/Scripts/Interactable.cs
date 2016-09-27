08bb8925a6eb4702a02dcabb51e54a11b654e7e1
Boothand <boothand@hotmail.com>
2016-09-27 21:06:30 +0200

    Trigger system start

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+	[SerializeField]
+	protected bool toggle;
+
+	void Start()
+	{
+
+	}
+
+	public virtual void ReceiveSignal(int signal)
+	{
+
+	}
+
+	void Update()
+	{
+
+	}
+}
---
+++ Assets/Scripts/Trigger2D.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Trigger2D : MonoBehaviour
+{
+	[SerializeField]
+	protected LayerMask triggerLayers;
+
+	[SerializeField]
+	protected Interactable target;
+
+	[SerializeField]
+	protected bool useButton;
+
+	[SerializeField]
+	protected int signal = 1;
+
+	[SerializeField]
+	protected bool retriggerBeforeExit;
+
+	[SerializeField]
+	protected float triggerRate = 1f;
+
+	protected bool someoneInside;
+
+	float retriggerTimer;
+
+	public bool getUseButton { get { return useButton; } }
+	public Interactable getTarget { get { return target; } }
+
+	void Start()
+	{
+
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if ((1 << (col.gameObject.layer) & triggerLayers) == 0 ||
+			useButton ||
+			!target)
+		{
+			return;
+		}
+
+		SendSignal(target);
+	}
+
+	void OnTriggerStay2D(Collider2D col)
+	{
+		if ((1 << (col.gameObject.layer) & triggerLayers) == 0 ||
+			useButton ||
+			!target)
+		{
+			return;
+		}
+
+		if (retriggerBeforeExit)
+		{
+			retriggerTimer += Time.deltaTime;
+
+			if (retriggerTimer >= triggerRate)
+			{
+				SendSignal(target);
+				retriggerTimer = 0f;
+			}
+		}
+
+		someoneInside = true;
+	}
+
+	void OnTriggerExit2D(Collider2D col)
+	{
+		if ((1 << (col.gameObject.layer) & triggerLayers) == 0 ||
+			useButton)
+		{
+			return;
+		}
+
+		someoneInside = false;
+	}
+
+	protected virtual void SendSignal(Interactable target, int newSignal = -1)
+	{
+		if (newSignal == -1)
+		{
+			newSignal = signal;
+		}
+
+		target.ReceiveSignal(newSignal);
+	}
+
+	void Update()
+	{
+		
+	}
+}
